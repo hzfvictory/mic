96ed58646a9f08288834c686c7cee60bf23ee596
hezhenfeng <feng960106@163.com>
2020-08-31 14:23:45 +0800

    :tada:

---
+++ micro-app-main/config-overrides.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const packageName = require('./package.json').name;
+
+module.exports = {
+  webpack: (config) => {
+    // https://webpack.js.org/configuration/output/#outputlibrary
+
+    // 微应用的包名，这里与主应用中注册的微应用名称一致
+    config.output.library = `${packageName}-[name]`;
+    // 将你的 library 暴露为所有的模块定义下都可运行的方式
+    config.output.libraryTarget = "umd";
+    // 按需加载相关，设置为 webpackJsonp_VueMicroApp 即可
+    config.output.jsonpFunction = `webpackJsonp_${packageName}`
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+
+    if (process.env.ANALYZE) {
+      if (config.plugins) {
+        config.plugins.push(
+          new BundleAnalyzerPlugin()
+        );
+      }
+    }
+    return config;
+  },
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+      // 关闭主机检查，使微应用可以被 fetch
+      config.disableHostCheck = true;
+      // 配置跨域请求头，解决开发环境的跨域问题
+      config.headers = {
+        "Access-Control-Allow-Origin": "*",
+      };
+      // 配置 history 模式
+      config.historyApiFallback = true;
+
+      config.hot = true;
+      config.open = true;  // 子应用设置false
+      return config;
+    };
+  },
+};
